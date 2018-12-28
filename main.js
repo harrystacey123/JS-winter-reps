2cebe56c8940fabe64d6c670e49f83fdde309e96
harrystacey123 <harrystacey@live.co.uk>
2018-12-28 12:28:08 -0800

    finished C

--- main.js
+++ main.js
@@ -101,9 +101,20 @@ if (age >= 16) {
 
 // B
 
-for (let i = 1; i<=100; i++) {
-    if (i % 2 == 0) {
-        console.log(i + '<-- is an even number');
-    }
-};
+// for (let i = 1; i<=100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i + '<-- is an even number');
+//     }
+// };
+
+// C
+
+// for (let i = 0; i < 100; i++) {
+//     if (i % 5 == 0) {
+//         console.log('I found a ' + i + ' High Five!');
+//     } else if (i % 3 == 0) {
+//         console.log('I found a ' + i + ' Three is a crowd');
+//     }
+// };
+
 
