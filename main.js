01134ef37cf6c6c2afadf82317c4b1643f8fd17f
harrystacey123 <harrystacey@live.co.uk>
2018-12-28 11:06:27 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// 1. How do we assign a value to a variable? 
+
+var dog = 'Arnold'
+
+// 2. How do we change the value of a variable? 
+
+var dog = 'Bertie'
+
+// 3. How do we assign an existing variable to a new variable?
+
+var cat = 'Arnold'
+
+// 4. Remind me, what are `declare`, `assign`, and `define`?
+
+// declaring a variable is just declaring its existance and therefore its need for memory allocation, defining the variable is setting it equal to something (e.i. assigning it to a value)
+
+// 5. What is pseudocoding and why should you do it?
+
+// qseudocoding is coding a problem in words rather than syntax. It helps because it breaks down large problems into many smaller problems which are often easier to tackle.
+
+// 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+// 30% / 70%
